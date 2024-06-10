cf6e815830b4f48778ed508be631bcf2717d0a90
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-10 15:37:02 +0200

    js part.1 ok

--- 1. Projets de départ/4. Cookies/app.js
+++ 1. Projets de départ/4. Cookies/app.js
@@ -1,26 +1,38 @@
-const form = document.querySelector('form');
-const inputs = form.querySelectorAll('.input-group input');
-const displayCookieBtn = form.querySelector('.display-cookie-btn');
+const cookieForm = document.querySelector('form');
+const inputs = cookieForm.querySelectorAll('.input-group input');
+// const displayCookieBtn = cookieForm.querySelector('.display-cookie-btn');
 
 const handleValidation = (e) => {
-  console.log(e);
+  if (e.type === 'invalid') {
+    e.target.setCustomValidity('Ce champ ne peut être vide.');
+  } else if (e.type === 'input') {
+    e.target.setCustomValidity('');
+  };
 };
 
 inputs.forEach(input => {
-  input.addEventListener('invalid', handleValidation)
-  input.addEventListener('input', handleValidation)
+  input.addEventListener('invalid', handleValidation);
+  input.addEventListener('input', handleValidation);
 });
 
 
-// const handleSubmit = () => {
-//   e.preventDefault();
-//   createCookie();
-// };
+const handleForm = (e) => {
+  e.preventDefault();
+  const newCookie = {};
 
-// const createCookie = (e) => {
-//   const createCookieBtn = form.querySelector('.create-cookie-btn');
-//   console.log('coucou');
-// };
+  inputs.forEach(input => {
+    const nameAttribute = input.getAttribute('name');
+    newCookie[nameAttribute] = input.value;
+  })
+  console.log(newCookie);
 
-// form.addEventListener('submit', handleSubmit);
-// form.addEventListener('click', displayCookie);
+  newCookie.expires = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+  createCookie(newCookie);
+};
+
+const createCookie = (newCookie) => {
+  // const createCookieBtn = form.querySelector('.create-cookie-btn');
+  // console.log('coucou');
+};
+
+cookieForm.addEventListener('submit', handleForm);
