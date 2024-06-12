1af40d050f95e5a9090cf6f1b0b8e2470506573c
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-12 15:31:19 +0200

    ex 5 finished

--- 1. Projets de départ/5. AppMétéo/script.js
+++ 1. Projets de départ/5. AppMétéo/script.js
@@ -1,22 +1,48 @@
+const loader = document.querySelector('.loader-container');
+const errorInformation = document.querySelector('.error-information');
+
 const APIKey = 'ef0cfd25-8005-4203-962f-3777346a75c5';
 
-const fetchData = async () => {
-  const url = `http://api.airvisual.com/v2/nearest_city?key=${APIKey}`
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  displayData(data.data);
+const getWeatherData = async () => {
+  try {
+    const url = `http://api.airvisual.com/v2/nearest_city?key=${APIKey}`
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}, ${response.statusText}`);
+    };
+
+    const data = await response.json();
+
+    const sortedData = {
+      city: data.data.city,
+      country: data.data.country,
+      temperature: data.data.current.weather.tp,
+      iconId: data.data.current.weather.ic
+    };
+
+      displayData(sortedData);
+  } catch (error) {
+      loader.classList.remove('active');
+      errorInformation.textContent = error.message;
+  }
 };
+
+// document.querySelector('.get-data').addEventListener('click', getWeatherData);
 
 const cityName = document.querySelector('.city-name');
 const countryName = document.querySelector('.country-name');
 const temperature = document.querySelector('.temperature');
+const infoIcon = document.querySelector('.info-icon');
 
 const displayData = (data) => {
+  loader.classList.remove('active');
+
   cityName.textContent = data.city;
   countryName.textContent = data.country;
-  temperature.textContent = `${data.current.weather.tp}°`;
-  // comment recup image icon correspondante avec le svg ?
+  temperature.textContent = `${data.temperature}°`;
+  infoIcon.src = `ressources/icons/${data.iconId}.svg`;
+  infoIcon.style.width = '150px';
 };
 
-document.addEventListener('DOMContentLoaded', fetchData);
+document.addEventListener('DOMContentLoaded', getWeatherData);
