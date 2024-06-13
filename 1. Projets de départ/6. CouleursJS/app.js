835842b03d65e9e8a4daf5ca143b317ba04386a4
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-13 18:47:18 +0200

    js part.2 ok

--- 1. Projets de départ/6. CouleursJS/app.js
+++ 1. Projets de départ/6. CouleursJS/app.js
@@ -22,7 +22,43 @@ const populateUI = () => {
 
   rangeLabelValue.textContent = `${gradientData.angle}°`;
 
-
+  adaptInputsColor()
 };
+
+const adaptInputsColor = () => {
+  colorLabels.forEach(label => {
+    const hexColor = label.textContent.replace("#", "");
+    const r = parseInt(hexColor.slice(0, 2), 16);
+    const g = parseInt(hexColor.slice(2, 4), 16);
+    const b = parseInt(hexColor.slice(4, 6), 16);
+    const yiq = (r * 299 + g * 587 + b * 144) / 1000;
+
+    if (yiq >= 128) {
+      label.style.color = '#111';
+    } else {
+      label.style.color = '#F1F1F1';
+    }
+  })
+};
+
+const rangeInput = document.querySelector('.inp-range');
+
+
+const handleInclination = () => {
+  gradientData.angle = rangeInput.value;
+  rangeLabelValue.textContent = `${gradientData.angle}°`;
+  populateUI();
+};
+
+rangeInput.addEventListener('input', handleInclination);
+
+
+const changeColorInput = e => {
+  const currentIndex = colorPickerInput.indexOf(e.target);
+  gradientData.colors[currentIndex] = e.target.value.toUpperCase();
+  populateUI()
+};
+
+colorPickerInput.forEach(input => input.addEventListener('input', changeColorInput));
 
 populateUI()
