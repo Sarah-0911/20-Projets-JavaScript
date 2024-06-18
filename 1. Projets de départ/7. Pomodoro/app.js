7e342ba09e1a5ce6ce1b415e146a31ee5210f274
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-18 18:04:56 +0200

    js part.2 ok

--- 1. Projets de départ/7. Pomodoro/app.js
+++ 1. Projets de départ/7. Pomodoro/app.js
@@ -1,5 +1,8 @@
-let workTime = 1800;
-let restTime = 300;
+let workTime = 3;
+let restTime = 3;
+
+let currentInterval = false;
+let timerId;
 
 const formatedTime = (time) =>  {
   return `${Math.trunc(time / 60)}:${time % 60 < 10 ? '0' : ''}${time % 60}`;
@@ -8,20 +11,55 @@ const formatedTime = (time) =>  {
 const displayWork = document.querySelector('.work-time');
 const displayRest = document.querySelector('.rest-time');
 
-const displayTimes = () => {
+displayWork.textContent = formatedTime(workTime);
+displayRest.textContent = formatedTime(restTime);
+
+const togglePlayBtn = document.querySelector('.toggle-btn');
+
+const togglePomodoro = () => {
+  handlePlayPause()
+
+  if (currentInterval) return;
+
+  currentInterval = true;
+  workTime--;
   displayWork.textContent = formatedTime(workTime);
-  displayRest.textContent = formatedTime(restTime);
-}
-
-
-const startButton = document.querySelector('.start-btn');
-
-const handleTimer = () => {
-  const time = setTimeout(() => {
-    workTime--;
-  }, 1000);
-  displayTimes();
-  console.log(time);
+  timerId = setInterval(handleTicks, 1000);
 };
 
-startButton.addEventListener('click', handleTimer);
+let pause = true;
+const handlePlayPause = () => {
+  if (togglePlayBtn.getAttribute('data-toggle') === 'play') {
+    pause = false;
+    togglePlayBtn.firstElementChild.src = "ressources/pause.svg";
+    togglePlayBtn.setAttribute('data-toggle', 'pause');
+  } else {
+    pause = true;
+    togglePlayBtn.firstElementChild.src = "ressources/play.svg";
+    togglePlayBtn.setAttribute('data-toggle', 'play');
+  }
+};
+
+const cycles = document.querySelector('.cycles');
+let cyclesNumber = 0;
+
+const handleTicks = () => {
+  if (!pause && workTime > 0) {
+    workTime--;
+    displayWork.textContent = formatedTime(workTime);
+  }
+  else if (!pause && !workTime && restTime > 0) {
+    restTime--;
+    displayRest.textContent = formatedTime(restTime);
+  }
+  else if (!pause && !workTime && !restTime) {
+    workTime = 2;
+    restTime = 3;
+    displayWork.textContent = formatedTime(workTime);
+    displayRest.textContent = formatedTime(restTime);
+    cyclesNumber++
+    cycles.textContent = `Cycle(s): ${cyclesNumber}`;
+  }
+};
+
+togglePlayBtn.addEventListener('click', togglePomodoro);
