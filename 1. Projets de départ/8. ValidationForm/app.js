0c44b042b85ad7ca85d8e38d2f4e7faece0ede3c
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-20 10:15:24 +0200

    js part.1 ok

--- 1. Projets de départ/8. ValidationForm/app.js
+++ 1. Projets de départ/8. ValidationForm/app.js
@@ -2,8 +2,7 @@ const form = document.querySelector('form');
 const validationIcons = document.querySelectorAll('.icon-verif');
 const validationTexts = document.querySelectorAll('.error-msg');
 
-const userName = document.querySelector('#user');
-
+const userInput = document.querySelector('#user');
 
 // const handleForm = (e) => {
 //   e.preventDefault();
@@ -14,25 +13,41 @@ const userName = document.querySelector('#user');
 
 
 const userValidation = () => {
-  if (userName.value.length >= 3) {
-    validationIcons[0].style.display = 'inline';
-    validationIcons[0].src = "ressources/check.svg";
-    validationTexts[0].style.display = 'none';
+  if (userInput.value.length >= 3) {
+    showValidation({ index: 0, validation: true });
   } else {
-    validationIcons[0].style.display = 'inline';
-    validationIcons[0].src = "ressources/error.svg";
-    validationTexts[0].style.display = 'block';
+    showValidation({ index: 0, validation: false });
   }
 };
 
-// const checkValidationIcon = (isValid) => {
-//   if (isValid) {
-//     icon.src = "ressources/check.svg";
-//   } else {
-//     icon.src = "ressources/error.svg";
-//   }
-// };
+userInput.addEventListener('blur', userValidation);
+userInput.addEventListener('input', userValidation);
 
-userName.addEventListener('blur', userValidation);
-userName.addEventListener('input', userValidation);
+
+const emailInput = document.querySelector('#email');
+
+const emailValidation = () => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+  if (emailRegex.test(emailInput.value)) {
+    showValidation({ index: 1, validation: true });
+  } else {
+    showValidation({ index: 1, validation: false });
+  }
+};
+
+emailInput.addEventListener('blur', emailValidation);
+emailInput.addEventListener('input', emailValidation);
+
+const showValidation = ({index, validation}) => {
+  if (validation) {
+    validationIcons[index].style.display = 'inline';
+    validationIcons[index].src = "ressources/check.svg";
+    validationTexts[index].style.display = 'none';
+  } else {
+    validationIcons[index].style.display = 'inline';
+    validationIcons[index].src = "ressources/error.svg";
+    validationTexts[index].style.display = 'block';
+  }
+};
+
 // form.addEventListener('submit', handleForm);
