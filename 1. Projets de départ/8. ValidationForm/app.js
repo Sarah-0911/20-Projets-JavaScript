f27db4230ce6613bedacb8ce7be7f16341bb1fa0
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-20 12:29:59 +0200

    js in progress

--- 1. Projets de départ/8. ValidationForm/app.js
+++ 1. Projets de départ/8. ValidationForm/app.js
@@ -17,7 +17,7 @@ const userValidation = () => {
     showValidation({ index: 0, validation: true });
   } else {
     showValidation({ index: 0, validation: false });
-  }
+  };
 };
 
 userInput.addEventListener('blur', userValidation);
@@ -32,21 +32,76 @@ const emailValidation = () => {
     showValidation({ index: 1, validation: true });
   } else {
     showValidation({ index: 1, validation: false });
-  }
+  };
 };
 
 emailInput.addEventListener('blur', emailValidation);
 emailInput.addEventListener('input', emailValidation);
 
+
+const passwordInput = document.querySelector('#password');
+// const lines = document.querySelector('.lines');
+
+const passwordVerification = {
+  length: false,
+  symbol: false,
+  number: false
+};
+
+const regexList = {
+  symbol: /[^a-zA-Z0-9\s]/,
+  number: /[0-9]/
+};
+
+let passwordValue;
+
+const passwordValidation = () => {
+  passwordValue = passwordInput.value;
+  for (const prop in passwordVerification) {
+
+    if (prop === 'length') {
+      if (passwordValue.length < 6) {
+        passwordVerification.length = false;
+      } else {
+        passwordVerification.length = true;
+      }
+      continue;
+    }
+
+    if(regexList[prop].test(passwordValue)) {
+      passwordVerification[prop] = true;
+    } else {
+      passwordVerification[prop] = false;
+    }
+  }
+
+  console.log(passwordVerification);
+
+  // const passwordRegex = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).*$/;
+  // if (passwordInput.value.lenght < 6 && passwordRegex.test(passwordInput.value)) {
+  //   showValidation({ index: 2, validation: true })
+  //   if (passwordInput.value.lenght < 6) {
+  //     lines.style.display = 'inline';
+  //   }
+  // } else {
+  //   showValidation({ index: 2, validation: false })
+  // };
+};
+
+
+passwordInput.addEventListener('blur', passwordValidation);
+passwordInput.addEventListener('input', passwordValidation);
+
+
 const showValidation = ({index, validation}) => {
   if (validation) {
     validationIcons[index].style.display = 'inline';
     validationIcons[index].src = "ressources/check.svg";
-    validationTexts[index].style.display = 'none';
+    // validationTexts[index].style.display = 'none';
   } else {
     validationIcons[index].style.display = 'inline';
     validationIcons[index].src = "ressources/error.svg";
-    validationTexts[index].style.display = 'block';
+    // validationTexts[index].style.display = 'block';
   }
 };
 
