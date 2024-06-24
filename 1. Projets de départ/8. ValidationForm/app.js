68eaab3518babe77e6a8eb5b8a3d5f599a916f1e
Sarah-0911 <sarah.tabet69@gmail.com>
2024-06-24 14:35:44 +0200

    ex 8 finished

--- 1. Projets de départ/8. ValidationForm/app.js
+++ 1. Projets de départ/8. ValidationForm/app.js
@@ -6,29 +6,51 @@ const inputsValidity = {
 };
 
 const form = document.querySelector('form');
-const container = document.querySelector('container');
+const container = document.querySelector('.container');
 
 const validationIcons = document.querySelectorAll('.icon-verif');
 const validationTexts = document.querySelectorAll('.error-msg');
 
 const userInput = document.querySelector('#user');
 
-
 let isAnimating = false;
+
 const handleForm = (e) => {
   e.preventDefault();
+
   const keys = Object.keys(inputsValidity);
   const failedInputs = keys.filter(key => !inputsValidity[key]);
+
   if (failedInputs.length && !isAnimating) {
     isAnimating = true;
     container.classList.add('shake');
+
     setTimeout(() => {
       container.classList.remove('shake');
       isAnimating = false;
     }, 400);
+
+    failedInputs.forEach(input => {
+      const index = keys.indexOf(input);
+      showValidation({ index: index, validation: false });
+    })
+
   } else {
-    isAnimating = false;
+    alert('Données envoyées avec succès.')
   };
+
+};
+
+const showValidation = ({index, validation}) => {
+  if (validation) {
+    validationIcons[index].style.display = 'inline';
+    validationIcons[index].src = "ressources/check.svg";
+    if (validationTexts[index]) validationTexts[index].style.display = 'none';
+  } else {
+    validationIcons[index].style.display = 'inline';
+    validationIcons[index].src = "ressources/error.svg";
+    if (validationTexts[index]) validationTexts[index].style.display = 'block';
+  }
 };
 
 form.addEventListener('submit', handleForm);
@@ -170,16 +192,3 @@ const confirmPassword = () => {
 
 confirmInput.addEventListener('blur', confirmPassword);
 confirmInput.addEventListener('input', confirmPassword);
-
-
-const showValidation = ({index, validation}) => {
-  if (validation) {
-    validationIcons[index].style.display = 'inline';
-    validationIcons[index].src = "ressources/check.svg";
-    if (validationTexts[index]) validationTexts[index].style.display = 'none';
-  } else {
-    validationIcons[index].style.display = 'inline';
-    validationIcons[index].src = "ressources/error.svg";
-    if (validationTexts[index]) validationTexts[index].style.display = 'block';
-  }
-};
