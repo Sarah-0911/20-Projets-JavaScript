de71b17accfbb0f19f3e14d70a31901f4f937e93
Sarah-0911 <sarah.tabet69@gmail.com>
2024-07-02 15:19:06 +0200

    ex 11 finished

--- 1. Projets de départ/11. Slider/app.js
+++ 1. Projets de départ/11. Slider/app.js
@@ -2,22 +2,25 @@ const slides = [...document.querySelectorAll('.slide')];
 const directionBtns = [...document.querySelectorAll('.direction-btn')];
 
 const sliderData = {
+  locked: false,
   direction: 0,
   slideOutIndex: 0,
   slideInIndex: 0
 };
 
 const handleClick = (e) => {
+  if (sliderData.locked) return;
+  sliderData.locked = true;
+
   getDirection(e.target);
+
   slideOut();
   };
 
   const getDirection = (btn) => {
     sliderData.direction = btn.classList.contains('right') ? 1 : -1;
 
-    sliderData.slideOutIndex = slides.findIndex(slide => {
-      slide.classList.contains('active');
-    })
+    sliderData.slideOutIndex = slides.findIndex(slide => slide.classList.contains('active'));
 
     if (sliderData.slideOutIndex + sliderData.direction > slides.length - 1) {
       sliderData.slideInIndex = 0;
@@ -27,6 +30,25 @@ const handleClick = (e) => {
       sliderData.slideInIndex = sliderData.slideOutIndex + sliderData.direction;
     }
   };
+
+  const slideIn = (e) => {
+    slideAnimation({
+      el: slides[sliderData.slideInIndex],
+      props: {
+        transition: 'transform .4s ease-out, opacity .6s ease-out',
+        transform: `translateX(0%)`,
+        opacity: 1
+      }
+    })
+    slides[sliderData.slideInIndex].classList.add('active');
+    slides[sliderData.slideOutIndex].classList.remove('active');
+
+    e.target.removeEventListener('transitionend', slideIn);
+    slides[sliderData.slideOutIndex].style.display = 'none';
+    setTimeout(() => {
+      sliderData.locked = false;
+    }, 400);
+  }
 
   const slideOut = () => {
     slideAnimation({
@@ -43,9 +65,11 @@ const handleClick = (e) => {
       props: {
         transition: 'transform .4s cubic-bezier(0.74, -0.34, 1, 1.19), opacity .4s ease-out',
         transform: `translateX(${sliderData.direction < 0 ? '-100%' : '100%'})`,
-        opacity: 0
+        opacity: 1
       }
     })
+
+    slides[sliderData.slideOutIndex].addEventListener('transitionend', slideIn);
   };
 
   const slideAnimation = (animationObject) => {
