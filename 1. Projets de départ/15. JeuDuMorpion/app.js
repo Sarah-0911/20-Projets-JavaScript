4681c07942ac332b04a08a458500e0b18c83b2e1
Sarah-0911 <sarah.tabet69@gmail.com>
2024-07-09 18:15:24 +0200

    js part.1 in progress

--- 1. Projets de départ/15. JeuDuMorpion/app.js
+++ 1. Projets de départ/15. JeuDuMorpion/app.js
@@ -1,5 +1,6 @@
-const currentGame = ["","","","","","","","",""];
+const cells = document.querySelectorAll('.cell');
 
+const currentGame = ["","","","","","","","",""];
 
 const winningCombinations = [
     [0, 1, 2],
@@ -11,3 +12,30 @@ const winningCombinations = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+
+const drawItem = (e) => {
+  console.log(e.target);
+
+  for (let i = 0; i < currentGame.length; i++) {
+    currentGame[i] = 'O';
+    e.target.textContent = 'O';
+
+    if (currentGame[i] = 'O') {
+      currentGame[i] += 'X';
+      e.target.textContent = 'X';
+
+    } else if (currentGame[i] = 'X') {
+      currentGame[i] += 'O';
+      e.target.textContent = 'O';
+    }
+  }
+  console.log(currentGame);
+
+  // if (condition) {
+  // } else {
+  //   cell.textContent = 'O';
+  // }
+};
+
+cells.forEach(cell => cell.addEventListener('click', drawItem));
