902be77e79b68e1e790d260352626572404659a2
Sarah-0911 <sarah.tabet69@gmail.com>
2024-07-10 18:56:14 +0200

    ex 15 finished

--- 1. Projets de départ/15. JeuDuMorpion/app.js
+++ 1. Projets de départ/15. JeuDuMorpion/app.js
@@ -1,4 +1,5 @@
 const cells = document.querySelectorAll('.cell');
+const info = document.querySelector('.info');
 
 const currentGame = ["","","","","","","","",""];
 
@@ -13,29 +14,52 @@ const winningCombinations = [
     [2, 4, 6]
 ];
 
+let currentPlayer = 'X';
+info.textContent = `Au tour de ${currentPlayer}`;
 
-const drawItem = (e) => {
-  console.log(e.target);
+let lock = false;
 
-  for (let i = 0; i < currentGame.length; i++) {
-    currentGame[i] = 'O';
-    e.target.textContent = 'O';
+const handleClick = (e) => {
+  const cellIndex = e.target.getAttribute('data-index');
 
-    if (currentGame[i] = 'O') {
-      currentGame[i] += 'X';
-      e.target.textContent = 'X';
+  if (currentGame[cellIndex] !== '' || lock) return;
 
-    } else if (currentGame[i] = 'X') {
-      currentGame[i] += 'O';
-      e.target.textContent = 'O';
-    }
-  }
-  console.log(currentGame);
+  currentGame[cellIndex] = currentPlayer;
+  e.target.textContent = currentPlayer;
 
-  // if (condition) {
-  // } else {
-  //   cell.textContent = 'O';
-  // }
+  verification();
 };
 
-cells.forEach(cell => cell.addEventListener('click', drawItem));
+const verification = () => {
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const combinationToCheck = winningCombinations[i];
+
+    const a = currentGame[combinationToCheck[0]];
+    const b = currentGame[combinationToCheck[1]];
+    const c = currentGame[combinationToCheck[2]];
+
+    if (a === '' || b === '' || c === '') {
+      continue;
+    } else if (a === b && b === c) {
+      info.textContent = `Le joueur ${currentPlayer} a gagné !
+      Appuyez sur F5 pour recommencer.`;
+      lock = true;
+      return;
+    }
+  }
+
+  if (!currentGame.includes('')) {
+    info.textContent = 'Match nul ! Appuyez sur F5 pour recommencer.';
+    lock = true;
+    return;
+  }
+  switchPlayer();
+};
+
+const switchPlayer = () => {
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  info.textContent = `Au tour de ${currentPlayer}`;
+}
+
+
+cells.forEach(cell => cell.addEventListener('click', handleClick));
