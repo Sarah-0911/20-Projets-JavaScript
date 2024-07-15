887c85baee05ac7e172c9f3ddcb2a0c92c0dc542
Sarah-0911 <sarah.tabet69@gmail.com>
2024-07-15 15:57:10 +0200

    js part.2 ok

--- 1. Projets de départ/16. ParticulesJS/app.js
+++ 1. Projets de départ/16. ParticulesJS/app.js
@@ -1,6 +1,5 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-// console.log(ctx);
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -28,11 +27,58 @@ class Particle {
     } else if (this.y > canvas.height || this.y < 0) {
       this.directionY = -this.directionY;
     }
-    this.x =+ this.directionX;
-    this.y =+ this.directionY;
+    this.x += this.directionX;
+    this.y += this.directionY;
     this.draw();
   }
 }
 
-const particle1 = new Particle(10, 10, 50, 50, 2, '#f1f1f1');
-console.log(particle1);
+// const particle1 = new Particle(10, 10, 1, 1, 2, '#f1f1f1');
+// console.log(particle1);
+
+let particlesArray;
+
+const init = () => {
+  particlesArray = [];
+
+  const numberOfParticles = (canvas.height * canvas.width) / 7000;
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    // size allant de 1 à 3 (3 n'étant pas inclu);
+    const size = (Math.random() * 2) + 1;
+
+    // Math.random() * (max - min + 1) + min;
+    // 10 équivaudra à un padding de 10px;
+    const x = Math.random() * ((canvas.width - 10) - 10 + 1) + 10;
+    const y = Math.random() * ((canvas.height - 10) - 10 + 1) + 10;
+
+    const directionX = cleanDirection();
+    const directionY = cleanDirection();
+
+    particlesArray.push(new Particle(x, y, directionX, directionY, size, '#f1f1f1'));
+  }
+};
+
+const cleanDirection = () => {
+  // 0 ou 1;
+  const random = Math.floor(Math.random() * 2);
+  if (random) {
+    // de 0.5 à 1.5 non inclus;
+    return (Math.random() * 1) + 0.5;
+  } else {
+    // de -0.5 à -1.5 non inclus;
+    return (Math.random() * -1) - 0.5;
+  }
+};
+
+init();
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particlesArray.length; i ++) {
+    particlesArray[i].update();
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
