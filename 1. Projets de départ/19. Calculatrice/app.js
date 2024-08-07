60091ab6c6cdb773fb7dd00ea583d1331b2bc4bd
Sarah-0911 <sarah.tabet69@gmail.com>
2024-08-07 16:17:44 +0200

    js part.1 ok

--- 1. Projets de départ/19. Calculatrice/app.js
+++ 1. Projets de départ/19. Calculatrice/app.js
@@ -1,34 +1,94 @@
-const buttons = document.querySelectorAll('button');
-const calculation = document.querySelector('.calculation');
-const result = document.querySelector('.result');
+const calculatorData = {
+  calculation: '',
+  result: '',
+  displayedResult: false
+};
+
+const buttons = [...document.querySelectorAll('button[data-action]')];
+
+const digitsBtns = buttons.filter(button => /[0-9]/.test(button.getAttribute('data-action')));
+const operatorsBtns = buttons.filter(button => /[\/*+-]/.test(button.getAttribute('data-action')));
+
+const calculationDisplay = document.querySelector('.calculation');
+const resultDisplay = document.querySelector('.result');
+
+
+const handleDigits = (e) => {
+  const buttonValue = e.target.getAttribute('data-action');
+
+  if (calculatorData.calculation === '0') calculatorData.calculation = '';
+
+  calculatorData.calculation += buttonValue;
+  resultDisplay.textContent = calculatorData.calculation;
+
+  console.log(calculatorData.calculation);
+
+};
+
+const handleOperators = (e) => {
+  const buttonValue = e.target.getAttribute('data-action');
+
+  if (!calculatorData.calculation && buttonValue === '-') {
+    calculatorData.calculation += buttonValue;
+    resultDisplay.textContent = calculatorData.calculation;
+    return;
+  }
+  else if (!calculatorData.calculation || calculatorData.calculation === '-') return;
+  else if (calculatorData.calculation.slice(-1).match(/[\/*+-]/)) {
+    calculatorData.calculation = calculatorData.calculation.slice(0, -1) + buttonValue;
+    resultDisplay.textContent = calculatorData.calculation;
+  }
+  else {
+    calculatorData.calculation += buttonValue;
+    resultDisplay.textContent = calculatorData.calculation;
+    console.log(calculatorData.calculation);
+  }
+};
+
+
+digitsBtns.forEach(button => button.addEventListener('click', handleDigits));
+operatorsBtns.forEach(button => button.addEventListener('click', handleOperators));
+
+
+
+
+//----------------------------------------------------------------------------
 
 const handleData = (e) => {
   const data = e.target.getAttribute('data-action');
-  calculData(data);
+  operator = isNaN(data);
+  console.log(operator);
+
+  // calculateData(previousInput, operator, currentInput);
   displayData(data);
 };
 
 
-const numbers = [];
-const calculData = (data) => {
-  numbers.push(data === parseInt('1') || data === parseInt('2'));
-  console.log(numbers);
+// const calculateData = (a, operator, b) => {
+//   switch (operator) {
+//     case '+':
+//       return a + b;
+//     case '-':
+//       return a - b;
+//     case '*':
+//       return a * b;
+//     case '/':
+//       return a / b;
+//     default:
+//       return 0;
+//   }
+// };
 
-  // switch (data) {
-  //   case '+':
-  //     console.log('un plus !');
-  //     a + b;
-  //     break;
-  //   case '-':
+// const displayData = (data) => {
+//   if (data !== 'c' && data !== 'ce') {
+//     // resetCalculator();
+//     digitsArray.push(data);
+//     resultDisplay.textContent = digitsArray.join('');
+//   }
+//   if (data === '=') {
+//     calculationDisplay.textContent = digitsArray.join('');
+//     resultDisplay.textContent = 0;
+//   }
+// };
 
-  //     break;
-  // }
-};
-
-dataArray = [];
-const displayData = (data) => {
-  dataArray.push(data);
-  result.textContent = dataArray.join('');
-};
-
-buttons.forEach(button => button.addEventListener('click', handleData));
+// buttons.forEach(button => button.addEventListener('click', handleData));
