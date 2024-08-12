55fad85a11b08dd89994660be16fcedc7cd3bcb9
Sarah-0911 <sarah.tabet69@gmail.com>
2024-08-12 18:41:07 +0200

    js part.1 ok

--- 1. Projets de départ/20. Player audio/script.js
+++ 1. Projets de départ/20. Player audio/script.js
@@ -5,3 +5,103 @@ const musicsData = [
   { title: "Lost-Colours", artist: "Fakear", id: 4 },
 ];
 
+const audio = document.querySelector('audio');
+
+const thumbnail = document.querySelector('.thumbnail');
+const musicTitle = document.querySelector('.music-title');
+const artistName = document.querySelector('.artist-name');
+const indexTxt = document.querySelector('.current-index');
+
+
+let currentMusicIndex = 1;
+
+const populateUI = ({ title, artist }) => {
+
+  thumbnail.src = `ressources/thumbs/${title}.png`;
+  audio.src = `ressources/music/${title}.mp3`;
+  musicTitle.textContent = title;
+  artistName.textContent = artist;
+  indexTxt.textContent = `${currentMusicIndex}/${musicsData.length}`;
+
+  fillVariablesDuration();
+};
+
+//------------------------------------------------------------
+
+const currentTime = document.querySelector('.current-time');
+const durationTime = document.querySelector('.duration-time');
+
+
+let current;
+let totalDuration;
+
+const fillVariablesDuration = () => {
+  if (Number.isNaN(audio.duration)) return;
+
+  current = audio.currentTime;
+  totalDuration = audio.duration;
+
+  formatedTime(current, currentTime);
+  formatedTime(totalDuration, durationTime);
+};
+
+const progressBar = document.querySelector('.progress-bar');
+console.log(progressBar.getBoundingClientRect().width);
+
+
+const updateProgress = () => {
+  current = audio.currentTime;
+  formatedTime(current, currentTime);
+
+  const progressPosition =  current / totalDuration;
+  progressBar.style.transform = `scaleX(${progressPosition})`;
+};
+
+// const handleBarProgress = (e) => {
+
+// };
+
+const formatedTime = (time, element) => {
+  const mins = Math.floor(time / 60);
+  const secs = Math.floor(time % 60);
+
+  element.textContent = `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+};
+
+audio.addEventListener('loadeddata', fillVariablesDuration);
+audio.addEventListener('timeupdate', updateProgress);
+// progressBar.addEventListener('click', handleBarProgress);
+
+//-----------------------------------------------------------
+
+const playButton = document.querySelector('.play-btn');
+
+const handlePlayPause = () => {
+  audio.paused ? play() : pause();
+};
+
+const play = () => {
+  audio.play();
+  playButton.firstElementChild.src = 'ressources/icons/pause-icon.svg';
+};
+
+const pause = () => {
+  audio.pause();
+  playButton.firstElementChild.src = 'ressources/icons/play-icon.svg';
+};
+
+playButton.addEventListener('click', handlePlayPause)
+
+//-----------------------------------------------------------
+
+populateUI(musicsData[currentMusicIndex - 1]);
+
+
+// const prevButton = document.querySelector('.prev-btn');
+// const nextButton = document.querySelector('.next-btn');
+
+// const choosePreviousMusic = () => {
+// };
+
+// prevButton.addEventListener('click', choosePreviousMusic);
+// nextButton.addEventListener('click', chooseNextMusic);
